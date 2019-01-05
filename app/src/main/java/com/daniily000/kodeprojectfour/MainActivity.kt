967db83fe2e5d8f71a3b0bddf60db537cb0040ab
daniily000 <daniily000@yandex.ru>
2019-01-06 00:47:56 +0200

    Searching by paradigms realized

--- app/src/main/java/com/daniily000/kodeprojectfour/MainActivity.kt
+++ app/src/main/java/com/daniily000/kodeprojectfour/MainActivity.kt
@@ -3,12 +3,20 @@ package com.daniily000.kodeprojectfour
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.View.GONE
+import com.daniily000.kodeprojectfour.data.Language
 import com.daniily000.kodeprojectfour.data.Languages
 import com.daniily000.kodeprojectfour.view.LanguageAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCurrentLanguagesList: MutableList<Language> = Languages.list.toMutableList()
 
     companion object {
         private const val SPAN_COUNT = 1
@@ -17,10 +25,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         languages_list.layoutManager = GridLayoutManager(this, SPAN_COUNT)
-        languages_list.adapter = LanguageAdapter(Languages.list)
+        languages_list.adapter = LanguageAdapter(mCurrentLanguagesList)
 
+        search_bar.clearFocus()
+
+        search_bar.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus && search_bar.text.isEmpty()) v.clearFocus()
+        }
+
+        search_bar.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {
+
+                mCurrentLanguagesList.clear()
+                Languages.list.forEach {
+                    if (it.searchParadigms(
+                            *search_bar.text.toString().trim().split(
+                                " ",
+                                ",",
+                                ignoreCase = true
+                            ).toTypedArray()
+                        )
+                    ) mCurrentLanguagesList.add(it)
+                }
+                languages_list.adapter?.notifyDataSetChanged()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+
+        R.id.action_search -> {
+
+            if (search_bar.visibility == View.GONE) {
+                search_bar.visibility = View.VISIBLE
+                search_bar.requestFocus()
+            } else {
+                search_bar.visibility = GONE
+                search_bar.setText("")
+                search_bar.clearFocus()
+            }
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/com/daniily000/kodeprojectfour/data/Language.kt
+++ app/src/main/java/com/daniily000/kodeprojectfour/data/Language.kt
@@ -1,5 +1,6 @@
 package com.daniily000.kodeprojectfour.data
 
+import android.util.Log
 import java.net.URL
 import java.util.*
 
@@ -10,7 +11,8 @@ data class Language(
     val paradigms: Set<Paradigm>,
     val tiobeIndex: Double,
     val infoPage: URL,
-    val xmlName: String) {
+    val xmlName: String
+) {
 
     /**
      * Returns authors as a readable string
@@ -27,4 +29,27 @@ data class Language(
         Arrays.toString(this.paradigms.toTypedArray())
             .replace("[", "")
             .replace("]", "")
+
+    fun searchParadigms(vararg keywords: String): Boolean {
+        Log.d(TAG, "Searching in ${paradigms()}")
+
+        for (keyword in keywords) {
+
+            Log.d(TAG, "keyword=$keyword")
+            var paradigmFound = false
+            for (paradigm in paradigms) {
+                Log.d(TAG, "paradigm=$paradigm")
+                paradigmFound = paradigmFound or paradigm.toString().contains(keyword, true)
+                Log.d(TAG, "paradigmFound=$paradigmFound\n")
+                if (paradigmFound) break
+            }
+            if (!paradigmFound) return false
+
+        }
+        return true
+    }
+
+    companion object {
+        private const val TAG = "Language"
+    }
 }
